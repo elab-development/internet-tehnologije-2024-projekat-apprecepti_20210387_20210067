d15d6d79383e8b45cb927aaf6821df3cc3f54c29
Neda Zdravkovic <nzdravkovicc@gmail.com>
2025-03-03 17:46:48 +0100

    Izmene u RecipeSeeder-u

--- receptiApp/database/seeders/RecipeSeeder.php
+++ receptiApp/database/seeders/RecipeSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Ingredient;
 use App\Models\Recipe;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +15,18 @@ class RecipeSeeder extends Seeder
      */
     public function run(): void
     {
-        Recipe::factory(20)->create();
+        Recipe::factory(20)->create()->each(function ($recipe) {
+            // Svaki recept pripada 1-3 kategorije
+            $recipe->categories()->attach(
+                Category::inRandomOrder()->limit(rand(1, 3))->pluck('id')->toArray()
+            );
+    
+            // Svaki recept dobija 3-6 sastojaka sa količinama
+            $recipe->ingredients()->attach(
+                Ingredient::inRandomOrder()->limit(rand(3, 6))->get()->mapWithKeys(function ($ingredient) {
+                    return [$ingredient->id => ['kolicina' => rand(50, 500), 'mera' => 'g']];
+                })->toArray()
+            );
+        });
     }
 }
